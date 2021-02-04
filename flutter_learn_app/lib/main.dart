d11b9d1693ddc34d01dde0fd750d6468f97ada10
lijiaxing <lijiaxing@whale.ws>
2021-02-04 19:47:03 +0800

    init

---
+++ flutter_learn_app/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import 'customwidget.dart';
+import 'newroute.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      routes: {
+        "new_page": (context) =>
+            NewRoute(text: ModalRoute.of(context).settings.arguments),
+      },
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            RandomWordsWidget(),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            FlatButton(
+              child: Text("open new route"),
+              textColor: Colors.blue,
+              onPressed: () async {
+                //导航到新路由
+                // var result = await Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) {
+                //   return NewRoute(text: "我是传过来的数据");
+                // }));
+                // print("收到的数据=$result");
+
+                Navigator.pushNamed(context, "new_page", arguments: "hi");
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
